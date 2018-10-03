427b79e62da67e2793e690cf400e832bcea7f09e
paulapivat <paul.apivat@gmail.com>
2018-10-03 16:55:13 +0700

    add input box for user comments

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -3,7 +3,10 @@ import React from 'react';
 const CommentSection = props => {
     return (
         <div>
-           Username: {props.username}, Second Text: {props.text}
+           Username: {props.username}, Second Text: {props.text},
+           <>
+             Input Box: <input placeholder="placeholder text"></input>
+           </>
         </div>
     )
 }

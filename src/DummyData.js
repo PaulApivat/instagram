d07e7d523a99236fff28bec9832480924427a3a7
paulapivat <paul.apivat@gmail.com>
2018-10-03 23:05:48 +0700

    install reactstrap, add bootstrap, implement bootstrap card in DummyData.js

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,29 +1,52 @@
 import React from 'react';
 import PostContainer from './components/PostContainer/PostContainer'
 import PropTypes from 'prop-types';
-//import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+
+// const DummyData = props => {
+//     return (
+//         <>
+//             {props.dummyData.map(e => (
+            
+//                     <div>
+//                         <>
+//                             <img src="#" />
+//                         </>
+//                         <>
+//                             <PostContainer commentsarray={e.comments}/>
+//                         </>
+//                         <>
+//                             <input placeholder="Add a comment..."></input>
+//                         </>
+//                     </div>
+                
+//             ))}
+//         </>
+//     )
+// }
 
 const DummyData = props => {
     return (
         <>
             {props.dummyData.map(e => (
-            
                     <div>
-                        <>
-                            <img src="#" />
-                        </>
-                        <>
-                            <PostContainer commentsarray={e.comments}/>
-                        </>
-                        <>
-                            <input placeholder="Add a comment..."></input>
-                        </>
+                        <Card>
+                            <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+                            <CardBody>
+                                <>
+                                    <PostContainer commentsarray={e.comments}/>
+                                </>
+                                <>
+                                    <input placeholder="Add a comment..."></input>
+                                </>
+                            </CardBody>
+                        </Card>
                     </div>
-                
             ))}
         </>
     )
 }
+
 
 DummyData.propTypes = {
     dummyData: PropTypes.array.isRequired

07e91493504bcd6bffad6378771fe0a544ef9ccb
paulapivat <paul.apivat@gmail.com>
2018-10-04 10:16:06 +0700

    created two separate folders ImageContainer and ThumbNailImageContainer for two separate components

--- src/DummyData.js
+++ src/DummyData.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostContainer from './components/PostContainer/PostContainer'
 import ImageContainer from './components/ImageContainer/ImageContainer'
+import ThumbNailImageContainer from './components/ThumbNailImageContainer/ThumbNailImageContainer'
 import PropTypes from 'prop-types';
 import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
 import './DummyData.css';
@@ -12,9 +13,9 @@ const DummyData = props => {
             {props.dummyData.map(e => (
                     <div>
                         <Card className="main-container">
-                            <CardImg className="thumbnail-image" src="#"/>
+                            <CardImg className="thumbnail-image" src={<ThumbNailImageContainer thumbnail={e.thumbnailUrl}/>} alt="Thumbnail Image"/>
                             <CardImg className="image" top width="100%" 
-                                src={<ImageContainer image={e.imageUrl} />} alt="Card image cap" 
+                                src={<ImageContainer image={e.imageUrl} />} alt="Main Image" 
                                 //"https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" 
                             />
                             <PostContainer 

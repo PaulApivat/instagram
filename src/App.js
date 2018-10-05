2ef743d22e1ba01284ab5168c4a1f6a28fae172f
paulapivat <paul.apivat@gmail.com>
2018-10-05 10:52:11 +0700

    use componentDidMount() to set data to component's state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@ import './App.css';
 import dummyData from './dummy-data';
 import DummyData from './DummyData';
 
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <DummyData dummyData={dummyData} />
+//       </div>
+//     );
+//   }
+// }
+
+// In app.js use componentDidMount() to set your data to the component's state. 
+// Use the state object to pass data to your PostContainer component.
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <DummyData dummyData={dummyData} />
-      </div>
-    );
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
   }
+
+  componentDidMount(){
+    console.log('mounting data...')
+    setTimeout(() => {
+      this.setState({data: dummyData});
+    }, 500);
+  }
+
+  render(){
+    return(
+      <div className="App">
+        <DummyData dummyData={this.state.data} />
+      </div>
+    )
+  }
+
 }
 
 export default App;

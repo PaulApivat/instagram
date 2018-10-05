b2caec7d9a65afd9f378e284cc17496aa126737c
paulapivat <paul.apivat@gmail.com>
2018-10-05 15:15:21 +0700

    change CommentSection component to Class component, pass incoming comment props to state

--- src/components/CommentSection/CommentSection.js
+++ src/components/CommentSection/CommentSection.js
@@ -11,9 +11,32 @@ import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } fr
 //     )
 // }
 
+// class CommentSection extends React.Component {
+//     constructor(){
+//         super();
+//     }
+
+//     render(){
+//         return(
+//             <CardBody>
+//                 <CardText><strong>{this.props.username}</strong> {this.props.text} </CardText>
+//             </CardBody>
+//         )
+//     }
+// }
+
 class CommentSection extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+            username: 'hard coded',
+            text: ''
+        };
+    }
+
+    componentDidMount(){
+        this.setState({username: `{this.props.username}`, text: `{this.props.text}`})
+        console.log('mounting CommentSection data...')
     }
 
     render(){
@@ -25,40 +48,6 @@ class CommentSection extends React.Component {
     }
 }
 
-
-// class CommentSection extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             username: 'hard coded'
-//         };
-//     }
-
-//     componentDidMount(){
-//         console.log('mounting CommentSection data...')
-//         this.props.setState({username: this.props.username});
-//     }
-// }
-
-// class CommentSection extends React.Component {
-//     constructor(props){
-//         super(props);
-//         this.state = {
-//             comments: [
-//                 props.username: 'HardCoded Name',
-//                 props.text: '',
-//             ]
-//         };
-//     }
-
-//     componentDidMount(){
-//         console.log('mounting CommentSection data...')
-//         let newComments = {...this.state.comments}
-//         props.comments.username = 'HardCoded Name';
-//         props.comments.text = '';
-//         this.props.setState({props.comments});
-//     }
-// }
 
 CommentSection.propTypes = {
     username: PropTypes.string.isRequired,

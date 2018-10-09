12e490acac0a8db96992e1c4459107aef59d9d93
paulapivat <paul.apivat@gmail.com>
2018-10-09 23:24:48 +0700

    add PostPage.js to PostContainer directory

---
+++ src/components/PostContainer/PostsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class PostsPage extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return (
+
+        )
+    }
+}
+
+export default PostsPage

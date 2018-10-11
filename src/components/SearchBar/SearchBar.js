cdf0116d134eb0d63d122460e6f83a9000eb6199
paulapivat <paul.apivat@gmail.com>
2018-10-11 22:44:06 +0700

    install styled components, in progress refactoring search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,14 +5,29 @@ import './Instagram-Letter.png'
 import './like.png';
 import './compass.png';
 import './person.png';
-import './search-icon.png';
+// import styled from 'styled-components';
+
+
 
 const instaLogo = './instagram-logo.png';
 const instaLetter = './Instagram-Letter.png';
 const likeImg = './like.png';
 const compass = './compass.png';
 const person = './person.png';
-const searchIcon = './search-icon.png';
+
+// const HeaderWrapper = styled.div`
+//     border-top: 3px solid gray;
+//     border-bottom: 3px solid gray;
+//     height: 100px;
+//     max-width: 1000px;
+//     width: 100%;
+//     margin: 0 auto;
+
+//     display: flex;
+//     justify-content: space-between;
+//     padding-top: 20px;
+// `
+
 
 class SearchBar extends React.Component {
     constructor(){
@@ -50,3 +65,17 @@ class SearchBar extends React.Component {
 }
 
 export default SearchBar
+
+/* <div className="searchbar-container">
+                <div>
+                    <img className="instalogo" src={instaLogo} alt="insta logo"/> | <img className="instaletter" src={instaLetter} alt="Instagram" />
+                </div>
+                <div className="searchbar">
+                    <input value={this.state.searchterm} onChange={this.handleChange} placeholder="Search" />
+                </div>
+                <div className="icon-cluster">
+                    <img className="compass" src={compass} alt="compass"/> 
+                    <img className="" src={likeImg} alt="heart"/> 
+                    <img className="person" src={person} alt="person"/>
+                </div>
+</div> */

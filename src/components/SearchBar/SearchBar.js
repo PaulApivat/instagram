dfd83e2589bca3bcd445e0ac40e80dd74f1452dd
paulapivat <paul.apivat@gmail.com>
2018-10-12 08:41:11 +0700

    create HeaderWrapper styled-component to wrap entire SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -15,24 +15,24 @@ const likeImg = './like.png';
 const compass = './compass.png';
 const person = './person.png';
 
-const SampleDiv = styled.div`
-    border: 1px solid red;
-    background-color: green;
-    color: white;
-`;
-
-// const HeaderWrapper = styled.div`
-//     border-top: 3px solid gray;
-//     border-bottom: 3px solid gray;
-//     height: 100px;
-//     max-width: 1000px;
-//     width: 100%;
-//     margin: 0 auto;
-
-//     display: flex;
-//     justify-content: space-between;
-//     padding-top: 20px;
+// const SampleDiv = styled.div`
+//     border: 1px solid red;
+//     background-color: green;
+//     color: white;
 // `;
+
+const HeaderWrapper = styled.div`
+    border-top: 3px solid gray;
+    border-bottom: 3px solid gray;
+    height: 100px;
+    max-width: 1000px;
+    width: 100%;
+    margin: 0 auto;
+
+    display: flex;
+    justify-content: space-between;
+    padding-top: 20px;
+`;
 
 
 class SearchBar extends React.Component {
@@ -53,7 +53,8 @@ class SearchBar extends React.Component {
 
     render(){
         return(
-            <div className="searchbar-container">
+            // <div className="searchbar-container">
+            <HeaderWrapper>
                 <div>
                     <img className="instalogo" src={instaLogo} alt="insta logo"/> | <img className="instaletter" src={instaLetter} alt="Instagram" />
                 </div>
@@ -65,10 +66,11 @@ class SearchBar extends React.Component {
                     <img className="" src={likeImg} alt="heart"/> 
                     <img className="person" src={person} alt="person"/>
                 </div>
-                <SampleDiv>
+                {/* <SampleDiv>
                     <div> New Div </div>
-                </SampleDiv>
-            </div>
+                </SampleDiv> */}
+            </HeaderWrapper>
+            // </div>
         )
     }
 }
